e1faf15f49e128aab3aaba069a2e4fe81c610741
ArturRibeiroDev <sk8arturzinho@gmail.com>
2026-03-17 14:16:20 -0300

    Projeto Organizado

--- sort-number/script.js
+++ sort-number/script.js
@@ -1,3 +1,4 @@
+// Declarei as variáveis padrões
 const form = document.querySelector("form")
 const numbers = document.getElementById("numbers")
 const FirstNumber = document.getElementById("first-number")
@@ -9,11 +10,14 @@ const again = document.querySelector("aside button")
 
 const ul = document.querySelector("ul")
 
+// Não deixa escrever letra
 numbers.oninput = () => {
   let value = numbers.value.replace(/\D/g, "")
 
   numbers.value = value
 }
+
+// Não deixa escrever letra
 
 FirstNumber.oninput = () => {
   let value = FirstNumber.value.replace(/\D/g, "")
@@ -21,44 +25,56 @@ FirstNumber.oninput = () => {
   FirstNumber.value = value
 }
 
+// Não deixa escrever letra
 SecondNumber.oninput = () => {
   let value = SecondNumber.value.replace(/\D/g, "")
 
   SecondNumber.value = value
 }
 
+// Quando eu der submite acontece isso
 form.onsubmit = (event) => {
+  // tira o padrão de form de atualizar a página
   event.preventDefault()
 
+  // Passei os valores para number
   const quantity = Number(numbers.value)
   const min = Number(FirstNumber.value)
   const max = Number(SecondNumber.value)
 
+  // Se o campo estiver vázio, não deixa passar
   if (!quantity || !min || !max) {
     alert("Preencha todos os campos!!! Por gentileza")
     return
   }
 
+  // Verificação padrão
   if (max <= min) {
     alert("O valor máximo deve ser maior do que o mínimo, tente novamente")
     return
   }
   
-  let Numbers_Sorted = SortNumbers(quantity, min, max)
+  // Transformo o valor da Array nos números sorteados
+  let Numbers_Sorted = SortEachNumbers(quantity, min, max)
 
   if (!Numbers_Sorted) return
 
+  // Quando clica para sortear, deixa o botão invisível
   again.classList.add("setInvisible")
 
+  // Para cara item na Array, ele vai ficar um elemento com o valor do Número, e se tiver 4 index por exemplo, 
+  // ele vai demorar 4 segundos pra cada 
   Numbers_Sorted.forEach((number, index) => {
     setTimeout(() => {
       createElement(number)
     }, index * 4000)
   })
 
+  // Depois de sortear tudo, ele traz o botão de volta
   setTimeout(() => {
     again.classList.remove("setInvisible")
 
+  // Esse é um timer para a animação do botão aparecendo
     setTimeout(() => {
       again.classList.add("show")
     }, 400)
@@ -66,7 +82,13 @@ form.onsubmit = (event) => {
 
 }
 
-function SortNumbers(numbers, FirstNumber, SecondNumber) {
+// Função que sorteia os números
+function Sort(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min
+}
+
+// Função que pega os números sorteados e põe na Array
+function SortEachNumbers(numbers, FirstNumber, SecondNumber) {
   let Numbers_Sorted = []
   if (repeat.checked) {
       const range = SecondNumber - FirstNumber + 1
@@ -76,14 +98,19 @@ function SortNumbers(numbers, FirstNumber, SecondNumber) {
     return
       }
   }
+  // Enquanto a quantidade de números sorteado for menor do que a quantidade de números a sortear, execulta
   while(Numbers_Sorted.length < numbers) {
     let NumberSorted = Sort(FirstNumber, SecondNumber)
+    // Se a opção de não repetir estiver ativada
     if (repeat.checked) {
       
-
+      // Aí se o número sorteado já estiver na Array
       if (Numbers_Sorted.includes(NumberSorted)) {
+        // Pula
         continue
-      } else {
+      }
+      // Se não, adiciona 
+      else {
       Numbers_Sorted.push(NumberSorted)
     }
     } else {
@@ -91,12 +118,10 @@ function SortNumbers(numbers, FirstNumber, SecondNumber) {
     }
   }
 
-return Numbers_Sorted
+  // Retorna a Array
+  return Numbers_Sorted
 }
 
-function Sort(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min
-}
 
 function createElement(value) {
 
@@ -123,7 +148,7 @@ again.addEventListener("click", () => {
   const min = Number(FirstNumber.value)
   const max = Number(SecondNumber.value)
 
-  let Numbers_Sorted = SortNumbers(quantity, min, max)
+  let Numbers_Sorted = SortEachNumbers(quantity, min, max)
 
   if (!Numbers_Sorted) return
 
